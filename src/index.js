e297d10bf5e008fb9c8841320bc7c90e9b779668
Atom <7548764@qq.com>
2019-08-05 22:49:25 +0800

    :tada:首次提交增加文件

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const config = {
+	dm : window.location.host,        // host 
+	pvid : "" ,		// userid
+	url : window.location.pathname ,       // url 	
+	arg : window.location.search ,      // url search
+	ty : 0 ,
+    v: "1.0.0",  // version
+    scr: window.screen.width + "x" + window.screen.height,   // devices screen
+    scl: window.screen.colorDepth + "-bit",   // devices colorDepth
+    lg: (navigator.language || navigator.userLanguage).toLowerCase(),  // language
+	tz: (new Date).getTimezoneOffset() / 60 ,  // time
+	d : +new Date, // now date
+	random : Math.random().toString().slice(2, 24),   // random number  
+};
+
+
+// const BASE_URL = "http://api.djui.cn/tongji/findnum?";
+const BASE_URL = 'http://localhost:3000/tongji/findnum?'; 
+
+const serialize = obj => {
+    var str = [];
+    for (var p in obj) 
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    return str.join("&");
+}
+
+const siteH5 = {};
+siteH5.init = function () {};
+siteH5.pgv = function () {
+	
+    const params = serialize(config);
+    const img = new Image;
+	img.onload = img.onabort = img.onerror = () => {
+		img.onload = img.onabort = img.onerror = null ;
+	}
+	img.src = BASE_URL + params;
+}
+
+siteH5.clickStat = function( evName ) {
+    const params = serialize(
+		Object.assign(config ,{
+			dm : "click",
+			url : evName
+		})
+	);
+	const img = new Image;
+	img.onload = img.onabort = img.onerror = () => {
+		img.onload = img.onabort = img.onerror = null ;
+	}
+	img.src = BASE_URL + params;
+}
+
+
+window.addEventListener('unload',function(){
+	const ua = this.navigator;
+	if( typeof navigator.sendBeacon !== "function" ) return ;
+	const params = serialize(
+		Object.assign(config ,{
+			d : +new Date()
+		})
+	);
+	ua.sendBeacon( BASE_URL + params );
+});
+
+
+// export default siteH5;
