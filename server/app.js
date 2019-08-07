b8757617c1a388486c637f10f1f1f7c883a54355
Atom <7548764@qq.com>
2019-08-08 01:39:49 +0800

    :construction: 工作进行时，更新代码

--- server/app.js
+++ server/app.js
@@ -3,16 +3,15 @@ const Router = require('koa-router');
 const koaBody = require('koa-body')();
 
 const Redis = require('./RedisInit');
-const DB = require('./DBinit');
+const { redis } = require('./RedisInit');
 
+const DB = require('./DBinit');
 const logsModel = require('./model/access_log');
 
 const app = new Koa();
 const router = new Router({
     prefix : '/tongji'
 });
-
-
 
 Redis.connect();
 DB.connect();
@@ -33,33 +32,17 @@ router.get('/findnum' , async ctx => {
     const ua = ctx.headers['user-agent'];
     const ip = getClientIp(ctx.req);
 
-    console.log( dm , pvid , uvmark , url , arg , v , scr , lg , tz );
-
-    await new logsModel({ 
-        dm , pvid , uvmark , url , arg , v , scr , lg , tz ,
-        creatime : Date.now() ,
-        ua , 
-        ip ,
-     }).save();
-
-    // await client.rpush("rpush", [ JSON.stringify( { dm , pvid , url , arg , v , scr , lg , tz , random } ) ]);
-
-    // const redisKey = "DATA_" + new Date().getHours();
-    // await client.rpush( redisKey , [ JSON.stringify( { dm , pvid , url , arg , v , scr , lg , tz , random } ) ]);
-
-    // const lists = await client.lrange( redisKey , 0 , -1 ,function (err, v) { 
-    //     console.log( err ,v  );
-    // });
-    // client.expire(redisKey , 1800000 );
-
+    const val = await redis.getAsync( 'UVMARK_' + uvmark );
+    if( val ) return ;
+    await redis.set('UVMARK_' + uvmark , uvmark ,'EX',  1 );
     
-});
+    const userdataStr = JSON.stringify( { dm , pvid , uvmark ,  url , arg , v , scr , lg , tz , ua , ip } );
+    const redisKey = "DATA_" + new Date().getHours();
+    await redis.rpush( redisKey , [ userdataStr ]);
 
-router.post('/findnum' , async ctx => {
-    ctx.type = "text/html;";
-    ctx.body = "";
-    const { dm , pvid , url , arg , v , scr , lg , tz , random } = ctx.request.query;
-    console.log( dm , pvid , url , arg , v , scr , lg , tz , random , "post" );
+    const lists = await redis.lrangeAsync( redisKey , 0 , -1 );
+    redis.expire(redisKey , 7200 );
+    
 });
 
 app.use(router.routes()).use(router.allowedMethods());
