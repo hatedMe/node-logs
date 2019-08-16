beb68fa0df96d8225f316ff5119c3ac76ae25e63
Atom <7548764@qq.com>
2019-08-16 20:39:01 +0800

    更新表结构

---
+++ server/model/environment.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose');
+
+const WebEnvironmentSchema = new mongoose.Schema({
+    app_id: { type: String }, // 所属系统
+    create_time: { type: Date, default: Date.now }, // 用户访问时间
+    url: { type: String }, // 访问页面的url
+    mark_page: { type: String }, // 所有资源页面统一标识 html img css js 用户系统信息等
+    mark_user: { type: String }, // 统一某一时间段用户标识
+    mark_uv: { type: String }, // 统一uv标识
+    browser: { type: String }, // 浏览器名称
+    borwser_version: { type: String }, // 浏览器版本
+    system: { type: String }, // 操作系统
+    system_version: { type: String }, // 系统版本
+    ip: { type: String }, // 访问者IP
+    county: { type: String }, // 国家
+    province: { type: String }, // 省
+    city: { type: String }, // 市
+}, {
+    shardKey: { _id: 'hashed' },
+    versionKey: false ,
+});
+
+WebEnvironmentSchema.index({ url: 1, create_time: -1 });
+WebEnvironmentSchema.index({ ip: 1, create_time: -1 });
+WebEnvironmentSchema.index({ create_time: -1 });
+WebEnvironmentSchema.index({ mark_page: 1 });
+WebEnvironmentSchema.index({ mark_user: 1 });
+
+
+module.exports = WebEnvironment = function(appId) {
+    return mongoose.model(`web_environment_${appId}`, WebEnvironmentSchema);
+};
